f46ae13283308877b2f0bd29cddc25671f38d664
prasannd52 <prasannadindi52@gmail.com>
2025-03-03 23:05:22 +0530

    rendering to home html file

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ applicaiton = Flask(__name__)
 app = applicaiton
 
 ## Route for a home page
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
